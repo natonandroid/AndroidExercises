6281d2cbfb5d3ab9b81a6d9249d6c7693e283d10
nataliac06 <nataliac@email.arizona.edu>
2018-04-05 14:28:22 -0700

    T06.01 Setup Settings in Visualizer

--- Lesson06-Visualizer-Preferences/T06.01-Exercise-SetupTheActivity/app/src/main/java/android/example/com/visualizerpreferences/VisualizerActivity.java
+++ Lesson06-Visualizer-Preferences/T06.01-Exercise-SetupTheActivity/app/src/main/java/android/example/com/visualizerpreferences/VisualizerActivity.java
@@ -17,6 +17,7 @@ package android.example.com.visualizerpreferences;
  */
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.example.com.visualizerpreferences.AudioVisuals.AudioInputReader;
 import android.example.com.visualizerpreferences.AudioVisuals.VisualizerView;
@@ -25,6 +26,9 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 public class VisualizerActivity extends AppCompatActivity {
@@ -44,15 +48,15 @@ public class VisualizerActivity extends AppCompatActivity {
 
     private void defaultSetup() {
         mVisualizerView.setShowBass(true);
-        mVisualizerView.setShowMid(true);
+        mVisualizerView.setShowMid(false);
         mVisualizerView.setShowTreble(true);
-        mVisualizerView.setMinSizeScale(1);
-        mVisualizerView.setColor(getString(R.string.pref_color_red_value));
+        mVisualizerView.setMinSizeScale(2);
+        mVisualizerView.setColor(getString(R.string.pref_color_blue_value));
     }
 
     /**
      * Below this point is code you do not need to modify; it deals with permissions
-     * and starting/cleaning up the AudioInputReader
+     * and starting/cleaning up the AudioInputReaderm
      **/
 
     /**
@@ -115,14 +119,33 @@ public class VisualizerActivity extends AppCompatActivity {
         }
     }
 
-    // TODO (1) Create a new Empty Activity named SettingsActivity; make sure to generate the
+    // (1) Create a new Empty Activity named SettingsActivity; make sure to generate the
     // activity_settings.xml layout file as well and add the activity to the manifest
 
-    // TODO (2) Add a new resource folder called menu and create visualizer_menu.xml
-    // TODO (3) In visualizer_menu.xml create a menu item with a single item. The id should be
+    // (2) Add a new resource folder called menu and create visualizer_menu.xml
+    // (3) In visualizer_menu.xml create a menu item with a single item. The id should be
     // "action_settings", title should be saved in strings.xml, the item should never
     // be shown as an action, and orderInCategory should be 100
 
-    // TODO (5) Add the menu to the menu bar
-    // TODO (6) When the "Settings" menu item is pressed, open SettingsActivity
+    //  (5) Add the menu to the menu bar
+    //  (6) When the "Settings" menu item is pressed, open SettingsActivity
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+         getMenuInflater().inflate(R.menu.visualizer_menu, menu);
+         return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if (id == R.id.action_settings){
+            Intent startSetingsActivity = new Intent(this, SettingsActivity.class);
+            startActivity(startSetingsActivity);
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
 }
